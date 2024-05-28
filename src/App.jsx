23fffabce35d1d9cd25520046fd660a1007fca54
Chris <chris.conlon1993@gmail.com>
2024-05-28 08:49:58 -0400

    [noissue] - We're learning forgive the massive noissue commits here

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import { invoke } from '@tauri-apps/api'
 
 function App() {
+  const [greeting, setGreeting] = useState('');
+  // Function to call the greet command and update the state
+  const getGreeting = async (name) => {
+    try {
+      const response = await invoke('greet', { name });
+      setGreeting(response);
+    } catch (error) {
+      console.error('Error invoking greet command:', error);
+    }
+  };
+  // Call the function when the component mounts
+  React.useEffect(() => {
+    getGreeting('(TESTING RUST FUNCTION CALL)');
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +25,8 @@ function App() {
         <p>
           Hello World from <code>src/App.js</code>
         </p>
+        <p>{greeting}</p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
