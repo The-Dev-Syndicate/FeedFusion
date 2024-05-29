f590484d33ce659d5884ab651c4849d619f8755d
Chris <chris.conlon1993@gmail.com>
2024-05-29 10:56:48 -0400

    [FF-3] - just doing some formatting with our new niftty formatter no code changes at all

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import FeedList from './components/general/FeedList';
 import Message from './components/general/Message';
 import { useNavigate } from "react-router-dom";
 import Sidebar from './components/general/Sidebar';
+
 function App() {
   const navigate = useNavigate()
   return (
--- src/components/general/FeedList.jsx
+++ src/components/general/FeedList.jsx
@@ -4,10 +4,10 @@ export default function FeedList() {
     const getThisFromRustLater = ['Feed A', 'Feed B', 'Feed C', 'Feed D']
     return (
         <>
-        <h1>Feeds</h1>
-        <ul>
-            {getThisFromRustLater.map((item, index) => <li className="list-item" key={index}>{item}</li>)}
-        </ul>
+            <h1>Feeds</h1>
+            <ul>
+                {getThisFromRustLater.map((item, index) => <li className="list-item" key={index}>{item}</li>)}
+            </ul>
         </>
     );
 }
