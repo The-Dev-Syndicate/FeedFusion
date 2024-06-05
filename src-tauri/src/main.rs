b8fb51fa1d48e358e82c9609f54a3d607b48f699
Chris <chris.conlon1993@gmail.com>
2024-06-04 21:13:43 -0400

    [FF-13] - new component for persistent data on load and a fancy splash screen

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,9 +4,10 @@
 mod internal; // Declare the internal module
 
 use std::time::Duration;
+use tauri::Manager;
+use tauri::{CustomMenuItem, Menu, MenuItem, Submenu};
 
 use internal::feed::FeedType;
-use tauri::{CustomMenuItem, Menu, MenuItem, Submenu};
 
 // TODO: We should add some configuration files so that the menu / port / etc. are customizable via config
 
@@ -14,27 +15,41 @@ fn main() {
     let menu = generate_menu();
     tauri::Builder::default()
         .setup(|app| {
-          let app_handle = app.handle();
-          println!("Setting up the app...");
-      
-          // TODO: this will come from DB on startup not from requests
-          // Define the feeds with their respective poll intervals
-          let feeds = vec![
-            internal::feed::Feed {
-                url: "https://lorem-rss.herokuapp.com/feed?unit=day".to_string(),
-                feed_type: FeedType::RSS,
-                poll_interval: Duration::from_secs(60),
-            },
-            internal::feed::Feed {
-                url: "https://run.mocky.io/v3/d3d616ed-4780-41f9-915f-bce277ae0afe".to_string(), // this url may need to be regenerated every so often
-                feed_type: FeedType::ATOM,
-                poll_interval: Duration::from_secs(20) //(24 * 60 * 60), // 24 hours
-            },
-            // Add more feeds as needed
-        ];
-      
-          internal::feed::start_feed_fetcher(app_handle, feeds);
-          Ok(())
+            let splashscreen_window = app.get_window("splashscreen").unwrap();
+            let main_window = app.get_window("main").unwrap();
+            // we perform the initialization code on a new task so the app doesn't freeze
+            tauri::async_runtime::spawn(async move {
+                // initialize your app here instead of sleeping :)
+                println!("Initializing...");
+                std::thread::sleep(std::time::Duration::from_secs(5)); // FIXME: This is arbitrary time to wait for now but we can do any heavy lifting for DB stuff here
+                println!("Done initializing.");
+
+                // After it's done, close the splashscreen and display the main window
+                splashscreen_window.close().unwrap();
+                main_window.show().unwrap();
+            });
+
+            let app_handle = app.handle();
+            println!("Setting up the app...");
+
+            // TODO: this will come from DB on startup not from requests
+            // Define the feeds with their respective poll intervals
+            let feeds = vec![
+                internal::feed::Feed {
+                    url: "https://lorem-rss.herokuapp.com/feed?unit=day".to_string(),
+                    feed_type: FeedType::RSS,
+                    poll_interval: Duration::from_secs(60),
+                },
+                internal::feed::Feed {
+                    url: "https://run.mocky.io/v3/d3d616ed-4780-41f9-915f-bce277ae0afe".to_string(), // this url may need to be regenerated every so often
+                    feed_type: FeedType::ATOM,
+                    poll_interval: Duration::from_secs(20), //(24 * 60 * 60), // 24 hours
+                },
+                // Add more feeds as needed
+            ];
+
+            internal::feed::start_feed_fetcher(app_handle, feeds);
+            Ok(())
         })
         .menu(menu) // Use the built above menu
         // This handles the events that the menu creates
